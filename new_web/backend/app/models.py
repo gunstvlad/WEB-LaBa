6dd335161dafaff5e75920558bf6a73c9610a5c5
gunstvlad <nva27052005@yandex.ru>
2025-11-16 15:54:53 +0300

    models is ready

---
+++ new_web/backend/app/models.py
@@ -0,0 +1,54 @@
+from sqlalchemy import Column, Integer, String, \
+    Boolean, DateTime, Text, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=1)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    cart_items = relationship("CartItem", back_populates="user")
+    reviews = relationship("Review", back_populates="user")
+
+class Product(Base):
+    __tablename__ = "products"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    description = Column(Text)
+    price = Column(Float, nullable=False)
+    category = Column(String, nullable=False)  # 'bed', 'sofa', 'wardrobe'
+    image_url = Column(String)
+    in_stock = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+class CartItem(Base):
+    __tablename__ = "cart_items"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
+    quantity = Column(Integer, default=1)
+    
+    # Связи
+    user = relationship("User", back_populates="cart_items")
+    product = relationship("Product")
+
+class Review(Base):
+    __tablename__ = "reviews"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_name = Column(String, nullable=False)  # Кэшируем имя для отображения
+    rating = Column(Integer, nullable=False)  # 1-5
+    text = Column(Text, nullable=False)
+    is_approved = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
+    # Связи
+    user = relationship("User", back_populates="reviews")
